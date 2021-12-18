47e86511c72975ae05688526d54b3e1348214ed5
Travis <duytantran@hotmail.com>
2021-12-17 16:59:50 -0800

    move code to class PhonebookServer

--- py_test/phonebook/phonebook_server.py
+++ py_test/phonebook/phonebook_server.py
@@ -7,12 +7,25 @@
 # - the clients.
 import csv
 
-keys = ['name','number']
+class PhonebookServer:
+    def __init__(self):
+        self.data_list = list()
 
-def load_data_from_file():
-    ''' Load data from CSV file and store in a list for '''
-    
+    def load_data_from_file(self):
+        ''' Load data from CSV file and store in a list for '''
+        with open('data.csv', mode='r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                self.data_list.append(row)
 
+    def print_data(self):
+        for data in self.data_list:
+            print("Name: {}\nPhone: {}\n".format(data['name'], data['phone']))
 
+    def run(self):
+        self.load_data_from_file()
+        self.print_data()
 
 if __name__ == '__main__':
+    server = PhonebookServer()
+    server.run()
