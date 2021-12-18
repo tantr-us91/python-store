176089b78c8e1fe33b2732d65d5466c385362dc4
Travis <duytantran@hotmail.com>
2021-12-17 16:29:18 -0800

    initial phonebook project directory

---
+++ py_test/phonebook/phonebook_server.py
@@ -0,0 +1,18 @@
+# A simple client-server phonebook
+# - The program will load the data (name and phone number) from a text file
+#   and start the server.
+# - When the server receive a input name from the client, the server will look for
+#   that name and return the phone number.
+# - Make the server to handle multiple connections and process multiple request from
+# - the clients.
+import csv
+
+keys = ['name','number']
+
+def load_data_from_file():
+    ''' Load data from CSV file and store in a list for '''
+    
+
+
+
+if __name__ == '__main__':
